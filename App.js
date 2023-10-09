ded030933fe9e627be882eeaf78b89a6bc30aadc
OscarSanchez10 <oscaritomo54@gmail.com>
2023-10-09 04:22:50 -0700

    good

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, ImageBackground, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, Image, ImageBackground, StyleSheet } from 'react-native';
 import { Audio } from 'expo-av';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Slider from 'react-native-slider';
@@ -16,6 +16,11 @@ class MusicPlayerApp extends Component {
         'Blackpink - Shut Down': require('./assets/Blackpink-ShutDown.mp3'),
         'Lalisa - Money': require('./assets/Lalisa-Money.mp3'),
         'New Jeans - OMG': require('./assets/NewJeans-OMG.mp3'),
+      },
+      songImages: {
+        'Blackpink - Shut Down': require('./assets/shutdown.jpg'),
+        'Lalisa - Money': require('./assets/money.jpg'),
+        'New Jeans - OMG': require('./assets/omg.jpg'),
       },
       songOrder: ['Blackpink - Shut Down', 'Lalisa - Money', 'New Jeans - OMG'],
       currentSongIndex: 0,
@@ -88,7 +93,7 @@ class MusicPlayerApp extends Component {
     } catch (error) {
     }
   };
-  
+
 
   componentWillUnmount() {
     this.sound.unloadAsync();
@@ -97,13 +102,17 @@ class MusicPlayerApp extends Component {
   render() {
     return (
       <ImageBackground
-        source={require('./assets/fondo.jpg')}
+        source={require('./assets/negro.jpg')}
         style={styles.background}
       >
         <View style={styles.container}>
-          <Text style={{ fontSize: 25, fontWeight: 'bold', color: "white", marginBottom: 20 }}>
+          <Text style={{ fontSize: 25, fontWeight: 'bold', color: 'white', marginBottom: 20 }}>
             {this.state.currentSong}
           </Text>
+          <Image
+            source={this.state.songImages[this.state.currentSong]}
+            style={styles.songImage}
+          />
           <Slider
             style={styles.slider}
             value={this.state.position}
@@ -115,22 +124,24 @@ class MusicPlayerApp extends Component {
             maximumTrackTintColor="gray"
           />
           <View style={styles.controlsContainer}>
-            <Button
-              title="<<"
-              onPress={() => this.changeSong(false)}
+            <TouchableOpacity
               style={styles.button}
-            />
+              onPress={() => this.changeSong(false)}
+            >
+              <Text style={styles.buttonText}>{"<<"}</Text>
+            </TouchableOpacity>
             <Icon
               name={this.state.isPlaying ? 'pause' : 'play'}
               size={35}
               color="white"
               onPress={this.togglePlayPause}
             />
-            <Button
-              title=">>"
-              onPress={() => this.changeSong(true)}
+            <TouchableOpacity
               style={styles.button}
-            />
+              onPress={() => this.changeSong(true)}
+            >
+              <Text style={styles.buttonText}>{">>"}</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </ImageBackground>
@@ -152,16 +163,34 @@ const styles = StyleSheet.create({
   controlsContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
     marginTop: 20,
+    paddingHorizontal: 20,
   },
   button: {
-    width: 35,
-    height: 35,
+    width: 80,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    borderRadius: 25,
+  },
+  buttonText: {
+    fontSize: 25,
+    color: 'white',
   },
   slider: {
     width: '80%',
     marginTop: 20,
   },
+  songImage: {
+    width: 300,
+    height: 300,
+    borderRadius: 10,
+    resizeMode: 'cover',
+    marginBottom: 20,
+  },
 });
 
 export default MusicPlayerApp;
+
